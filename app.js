f596aa10a922dce730c05b32e94276e9cbc0a655
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2024-02-05 14:49:04 -0800

    Implement password validation

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 passwordInput = document.querySelector("#pwd");
-confirmPasswordInput = document.querySelector("confirm-pwd");
+confirmPasswordInput = document.querySelector("#confirm-pwd");
+errorMessage = document.querySelector(".error");
+submitButton = document.querySelector("button")
 
-passwordInput.addEventListener("input", () => {
-    console.log("pwd input has changed");
-    if (passwordInput.value != confirmPasswordInput) {
+submitButton.addEventListener("click", (event) => {
+    if (confirmPasswordInput.value != passwordInput.value) {
         console.log("Passwords do not match");
+        errorMessage.textContent = "* Passwords do not match";
+        passwordInput.classList.add("invalid");
+        confirmPasswordInput.classList.add("invalid");
+        event.preventDefault();
+    }
+    else if (passwordInput.value == '' || confirmPasswordInput == ''){
+        console.log("empty pass");
+        errorMessage.textContent = "* Please enter a password";
+        passwordInput.classList.add("invalid");
+        confirmPasswordInput.classList.add("invalid");
+        event.preventDefault();
+    }
+    else {
+        errorMessage.textContent = "";
+        passwordInput.classList.remove("invalid");
+        confirmPasswordInput.classList.remove("invalid");
     }
 });
